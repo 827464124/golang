3d4705b9e8d93966e09cb4aee39909b256037668
golang <827464124@qq.com>
2020-01-14 13:01:06 +0800

    Update quantitative.go

--- stock/result/quantitative.go
+++ stock/result/quantitative.go
@@ -8,6 +8,14 @@ import (
 	"time"
 	"../conf"
 )
+
+type Status struct {
+	lowestHighAvg bool
+	highestLowAvg bool
+	hasBuyIn bool
+	value float64
+	number int
+}
 func ConnDB() *sql.DB {
 
 	dsn := fmt.Sprintf("%s:%s@%s(%s:%d)/%s",conf.USERNAME,conf.PASSWORD,conf.NETWORK,conf.SERVER,conf.PORT,conf.DATABASE)
@@ -24,7 +32,7 @@ func ConnDB() *sql.DB {
 }
 func QueryData(db *sql.DB,code string)  {
 	var skt = new(struct2db.StockInfo)
-	rows, err := db.Query("select code,name,open,high,low,close ,ma20 from his_stock_info where code = ?", code)
+	rows, err := db.Query("select date,code,name,open,high,low,close ,ma20 from his_stock_info where code = ? order by date", code)
 	defer func() {
 		if rows != nil {
 			rows.Close() //可以关闭掉未scan连接一直占用
@@ -35,13 +43,37 @@ func QueryData(db *sql.DB,code string)  {
 		fmt.Printf("Query failed,err:%v", err)
 		return
 	}
+	var ST Status
+	ST.number = 100
 	for rows.Next() {
-		err = rows.Scan(&skt.Code,&skt.Name,&skt.Open,&skt.High,&skt.Low,&skt.Close,&skt.Ma20) //不scan会导致连接不释放
+		err = rows.Scan(&skt.Date,&skt.Code,&skt.Name,&skt.Open,&skt.High,&skt.Low,&skt.Close,&skt.Ma20) //不scan会导致连接不释放
 		if err != nil {
 			fmt.Printf("Scan failed,err:%v", err)
 			return
 		}
-		fmt.Print(skt)
+		if skt.Low > skt.Ma20 {
+			ST.lowestHighAvg  = true
+		}else{
+			ST.lowestHighAvg = false
+		}
+		if skt.High < skt.Ma20{
+			ST.highestLowAvg = true
+		}else{
+			ST.highestLowAvg = false
+		}
+		if ST.lowestHighAvg && ! ST.hasBuyIn {
+			ST.hasBuyIn = true
+			ST.value = float64(ST.number) * skt.Close
+			fmt.Println("buy in ",ST.value)
+		}
+		if !ST.lowestHighAvg && ST.hasBuyIn{
+			ST.hasBuyIn = false
+			ST.value = (float64(ST.number) * skt.Close) * (1-0.02)
+
+			fmt.Println("sale out ",ST.value)
+		}
+		//fmt.Print(skt)
+
 	}
 }
 
